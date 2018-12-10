5d63cad0cd3a14bd1b7d928028be9ec599ef8f93
emeryduh <emeryhjones@gmail.com>
2018-12-10 13:33:33 -0500

    Indexing and searching working
    - Will now index all extracted files to ElasticSearch
    - Added Jsoup as a dependency for traversing and manipulating XML
    - A basic search can be done for now.  More

--- src/main/java/com/itec4020/websearch/App.java
+++ src/main/java/com/itec4020/websearch/App.java
@@ -5,19 +5,24 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.zip.GZIPInputStream;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.ParseException;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.ResponseListener;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.*;
 
 /**
  * Assignment 3 for ITEC 4020
@@ -35,6 +40,7 @@ import org.elasticsearch.client.RestClient;
 public class App {
 	
 	RestClient rest;
+	final String ELASTIC_PATH = "/files/documents";
 	
     public static void main( String[] args ) {
         System.out.println( "Starting up " );
@@ -45,7 +51,15 @@ public class App {
 	public App() {
     	openREST();
     	try {
-			extractDocuments();
+    		// Decompresses all documents into .txt files
+			decompress();
+			
+			// Breaks down each .txt file into a separate document and indexes them
+			index();
+			
+			// Run an example search against the ElasticSearch database
+			search("foreign minorities, Germany", "What language and cultural differences impede the integration" + 
+					" of foreign minorities in Germany?");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -75,10 +89,78 @@ public class App {
 	/**
 	 * Extract document data from compressed files
 	 */
-	public void extractDocuments() {
+	public void decompress() {
 		decompressDirectory("WT01");
 		decompressDirectory("WT02");
 		decompressDirectory("WT03");
+	}
+	
+	/**
+	 * Index documents into ElasticSearch
+	 */
+	public void index() {
+		indexDocuments("WT01", 0);
+		indexDocuments("WT02", 1);
+		indexDocuments("WT03", 2);
+	}
+	
+	/**
+	 * Searches given the information
+	 */
+	public void search(String title, String content) {
+		try {
+			Request request = new Request("GET", ELASTIC_PATH + "/_search");
+			
+			XContentBuilder builder = jsonBuilder()
+					.startObject()
+					.array("_source", "title", "docno")
+					.startObject("query")
+					.startObject("bool")
+					.startArray("should")
+					.startObject()
+					.startObject("match")
+					.startObject("title")
+					.field("query", title)
+					.field("boost", 4)
+					.endObject()
+					.endObject()
+					.endObject()
+					.startObject()
+					.startObject("match")
+					.field("content", content)
+					.endObject() 	// end match
+					.endObject() 	// close open bracket
+					.endArray() 	// end should
+					.endObject() 	// end bool
+					.endObject() 	// end query
+					.endObject(); 	// close open bracket
+			String json = Strings.toString(builder);
+			System.out.println(json);
+			request.setJsonEntity(json);
+			request.addParameter("pretty", "true");
+			
+			rest.performRequestAsync(request,
+					new ResponseListener() {
+						public void onSuccess(Response response) {
+							System.out.println(response.getRequestLine());
+							
+							try {
+								System.out.println(EntityUtils.toString(response.getEntity()));
+							} catch (ParseException e) {
+								e.printStackTrace();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+
+						public void onFailure(Exception exception) {
+							System.out.println(exception);
+						}
+					}
+				);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -156,4 +238,180 @@ public class App {
 		});
 	}
 	
+	/**
+	 * TODO: Remove indexBump and replace with a more dynamic way to generate indicies
+	 * 
+	 * Index gathered document data
+	 * 
+	 * @param folder The folder name for the data where the folder name is placed
+	 * like so:  %PROJECT_DIR%/extracted/{folder}
+	 */
+	public void indexDocuments(String folder, int indexBump) {
+		System.out.println("Indexing folder: " + folder);
+		
+		// Setup the file path
+		String filePath = System.getProperty("user.dir") + "\\extracted\\" + folder;
+		
+		// Gather all files with the .txt extension in the file path
+		File[] files = getFilesByExt(filePath, ".txt");
+		
+		// CountDownLoatch for asynchronously requests to be used to identify when the next step can start
+		// TODO: Will be used later when other tasks are completed
+		final CountDownLatch latch = new CountDownLatch(files.length);
+		
+		for(int i = 0; i < files.length; i++) {
+			System.out.println("Indexing file " + i);
+			
+			// Gather all relevant JSONs for this file's documents
+			ArrayList<String> jsons = getJSONsFromPath(files[i].getPath());
+			int size = jsons.size();
+			
+			for(int j = 0; j < size; j++) {
+				// Assign each page an index assuming i are files, and j are documents
+				int documentIndex = (i * 10000) + j + (indexBump * 100000);
+				
+				// Build relevant requests
+				Request request = new Request("PUT", ELASTIC_PATH + "/" + documentIndex);
+				request.setJsonEntity(jsons.get(j));
+				
+				rest.performRequestAsync(request,
+					new ResponseListener() {
+						public void onSuccess(Response response) {
+							//System.out.println(response);
+							latch.countDown();
+						}
+
+						public void onFailure(Exception exception) {
+							System.out.println(exception);
+							latch.countDown();
+						}
+					}
+				);
+			}
+		}
+	}
+	
+	/**
+	 * Breaks apart an entire document that follows the format of
+	 * the decompressed files into separate JSON strings.  The strings
+	 * will be used into ElasticSearch.
+	 * 
+	 * @param path The complete path with file name to be read in.
+	 * @return An array of JSON strings to be used for indexing.
+	 */
+	public ArrayList<String> getJSONsFromPath(String path) {
+		// The String array to be passed back once complete
+		ArrayList<String> jsons = new ArrayList<String>();
+		
+		try {
+			// The entire document that is currently being parsed.
+			Document entireDoc = Jsoup.parse(new File(path), null);
+			
+			Element doc;
+			int i = 0;
+			
+			// Break each Document (<DOC>) down into it's own index
+			// Shift out the top-most DOC element to be processed
+			while((doc = entireDoc.select("doc").remove(i)) != null) {
+				jsons.add(getJSONFromData(doc));
+				i++;
+			}
+		} catch (NullPointerException errNull) {
+			errNull.printStackTrace();
+		} catch(IndexOutOfBoundsException errIndex) {
+			// This occurs when there are no more DOC tags to select, in which case return the JSONs
+			return jsons;
+		} catch (IOException errIO) {
+			errIO.printStackTrace();
+		}
+		
+		return jsons;
+	}
+	
+	/**
+	 * Extracts data from the given Element into the JSON format.  It is
+	 * expected that the Element provided was generated from Jsoup pulling out a 
+	 * single DOC tag.  (i.e. the root element must have no siblings and must be a
+	 * DOC tag)
+	 * 
+	 * @param e The DOC tag element with all information inside
+	 * @return The JSON string for the data provided
+	 */
+	public String getJSONFromData(Element e) {
+		try {
+			String title = "", docno = "", olddocno = "", keywords = "", content = "";
+			Element ele;
+			
+			// Only update the Strings if the elements exist
+			if((ele = e.select("title").first()) != null) {
+				title = ele.text();
+			}
+			
+			if((ele = e.select("docno").first()) != null) {
+				docno = ele.text();
+			}
+			
+			if((ele = e.select("docoldno").first()) != null) {
+				olddocno = ele.text();
+			}
+			
+			if((ele = e.select("meta").first()) != null) {
+				if(ele.hasAttr("name") && ele.attr("name") == "keywords") {
+					if(ele.hasAttr("content")) {
+						keywords = ele.attr("content");
+						System.out.println("FOUND KEYWORDS: " + keywords);
+					}
+				}
+			}
+			
+			// Gather only the body of the document as text content
+			if((ele = e.select("html").first()) != null) {
+				content = ele.text();
+			} else if((ele = e.select("body").first()) != null) {
+				content = ele.text();
+			} else if((ele = e.select("DOCHDR").first()) != null) {
+				// If the document is malformed and is missing opening html and body tags
+				// then instead strip out everything above and including the <DOCHDR>.
+				// Then set the content as all remaining text for this <DOC>.
+				int i = ele.elementSiblingIndex();
+				
+				// Recursively remove all siblings above the DOCHDR
+				removePreviousSiblings(ele, i);
+				content = e.text();
+			} else {
+				// In the unlikely scenario that the document is malformed and doesn't have an 
+				//html, body, or DOCHDR tag, then instead set the content as the entire <DOC>
+				content = e.text();
+			}
+			
+			// Build the json using the extracted data
+			XContentBuilder builder = jsonBuilder()
+					.startObject()
+					.field("title", title)
+					.field("docno", docno)
+					.field("olddocno", olddocno)
+					.field("keywords", keywords)
+					.field("content", content)
+					.endObject();
+			
+			return Strings.toString(builder);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Recursively removes the previous sibling until there are no more jumps
+	 * 
+	 * @param e The bottom-most element to start removing
+	 */
+	public void removePreviousSiblings(Element e, int jumpsLeft) {
+		if(jumpsLeft > 0) {
+			removePreviousSiblings(e.previousElementSibling(), jumpsLeft - 1);
+		}
+		
+		e.remove();
+	}
 }
